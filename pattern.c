9f5065c2b39c141fd1dd184a7cbad4a751da8501
saba <sabahusain2001@gmail.com>
2022-04-15 04:03:05 +0530

    checkerboard pattern

---
+++ pattern.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//drawing checkerboard pattern with 8 different print statement
+int main(void){
+    printf("* * * * * * * *\n");
+    printf(" * * * * * * * *\n");
+    printf("* * * * * * * *\n");
+    printf(" * * * * * * * *\n");
+    printf("* * * * * * * *\n");
+    printf(" * * * * * * * *\n");
+    printf("* * * * * * * *\n\n\n\n");
+
+    //making this pattern with lowest possible print statment
+    printf("* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n");
+}
