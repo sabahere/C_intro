4cd9a71dd432684842496755a6efd50a180daf42
saba <sabahusain2001@gmail.com>
2022-04-15 20:05:48 +0530

    Integer value of a character

---
+++ character.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(void){
+    int A, B, C, a, b, c, zero, one, two, dollar, asterisk, plus, forwardSlash;
+
+    A = 'A';
+    B = 'B';
+    C = 'C';
+    a = 'a';
+    b = 'b';
+    c = 'c';
+    zero = '0'; 
+    one = '1';
+    two = '2';
+    dollar = '$';
+    asterisk = '*';
+    plus = '+';
+    forwardSlash = '/';
+    printf("Integer vale of\nA = %i\n", A);
+    printf("B = %i\n", B);
+    printf("C = %i\n", C);
+    printf("a = %i\n", a);
+    printf("b = %i\n", b);
+    printf("c = %i\n", c);
+    printf("0 = %i\n", zero);
+    printf("1 = %i\n", one);
+    printf("2 = %i\n", two);
+    printf("$ = %i\n", dollar);
+    printf("* = %i\n", asterisk);
+    printf("/ = %i\n", forwardSlash);
+    
+}
