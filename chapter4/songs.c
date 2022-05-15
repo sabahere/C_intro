5b39a3f89283aa541c02ee0ea50b9a725d3f0bd3
saba <sabahusain2001@gmail.com>
2022-05-15 21:18:08 +0530

    THE TWELVE DAYS OF CHRISTMAS song

---
+++ chapter4/songs.c
@@ -0,0 +1,112 @@
+#include<stdio.h>
+int main(){
+    int count = 0;
+    int days;
+    int song = 1;
+    while(song<=12){
+
+            switch(song){
+            case 1:
+            printf("On the first ");
+            break;
+
+            case 2:
+            printf("On the second");
+            break;
+
+            case 3:
+            printf("On the third");
+            break;
+
+            case 4:
+            printf("On the fourth");
+            break;
+
+            case 5:
+            printf("On the fifth");
+            break;
+
+            case 6:
+            printf("On the sixth");
+            break;
+
+            case 7:
+            printf("On the seventh");
+            break;
+
+            case 8:
+            printf("On the eighth");
+            break;
+
+            case 9:
+            printf("On the nineth");
+            break;
+
+            case 10:
+            printf("On the tenth");
+            break;
+
+            case 11:
+            printf("On the eleventh");
+            break;
+
+            case 12:
+            printf("On the twelveth");
+            break;
+        }
+
+            switch(song){
+            case 1:
+            printf(" day of Christmas\nMy good friends brought to me\nA song and a Christmas tree\n\n");
+            break;
+
+            case 2:
+            printf(" day of Christmas\nMy good friends brought to me\nTwo candy canes\nAnd a song for the Christmas tree\n\n");
+            break;
+            
+            case 3:
+            printf(" day of Christmas\nMy good friends brought to me\nThree boughs of holly\nTwo candy canes\nAnd a song for the Christmas tree\n\n");
+            break;
+            
+            case 4:
+            printf(" day of Christmas\nMy good friends brought to me\nFour colored lights\nThree boughs of holly\nTwo candy canes\nAnd a song for the Christmas tree\n\n");
+            break;
+            
+            case 5:
+            printf(" day of Christmas\nMy good friends brought to me\nA shining star\nFour colored lights\nThree boughs of holly\nTwo candy canes\nAnd a song for the Christmas tree\n\n");
+            break;
+
+            case 6:
+            printf(" day of Christmas\nMy good friends brought to me\nLittle silver bells\nA shining star\nFour colored lights\nThree boughs of holly\nTwo candy canes\nAnd a song for the Christmas tree\n\n");
+            break;
+            
+            case 7:
+            printf(" day of Christmas\nMy good friends brought to me\nCandles a-glowing\nLittle silver bells\nA shining star\nFour colored lights\nThree boughs of holly\nTwo candy canes\nAnd a song for the Christmas tree\n\n");
+            break;
+            
+            case 8:
+            printf(" day of Christmas\nMy good friends brought to me\nGold and silver tinsel\nCandles a-glowing\nLittle silver bells\nA shining star\nFour colored lights\nThree boughs of holly\nTwo candy canes\nAnd a song for the Christmas tree\n\n");
+            break;
+
+            case 9:
+            printf(" day of Christmas\nMy good friends brought to me\nA guardian angel\nGold and silver tinsel\nCandles a-glowing\nLittle silver bells\nA shining star\nFour colored lights\nThree boughs of holly\nTwo candy canes\nAnd a song for the Christmas tree\n\n");
+            break;
+
+            case 10:
+            printf(" day of Christmas\nMy good friends brought to me\nSome mistletoe\nA guardian angel\nGold and silver tinsel\nCandles a-glowing\nLittle silver bells shining star\nFour colored lights\nThree boughs of holly\nTwo candy canes\nAnd a song for the Christmas tree\n\n");
+            break;
+            
+            case 11:
+            printf(" day of Christmas\nMy good friends brought to me\nGifts for one and all\nSome mistletoe\nA guardian angel\nGold and silver tinsel\nCandles a-glowing\nLittle silver bells\nA shining star\nFour colored lights\nThree boughs of holly\nTwo candy canes\nAnd a song for the Christmas tree\n\n");
+            break;
+
+            case 12:
+            printf(" day of Christmas\nMy good friends brought to me\nAll their good wishes\nGifts for one and all\nSome mistletoe\nA guardian angel\nGold and silver tinsel\nCandles a-glowing\nLittle silver bells\nA shining star\nFour colored lights\nThree boughs of holly\nTwo candy canes\nAnd a song for the Christmas tree\n\n");
+            break;
+            }
+
+            
+    
+    song++;
+    }
+}
