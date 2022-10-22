486fead0bbe0c67a9e513738ef6af94c0176d6a3
saba <sabahusain2001@gmail.com>
2022-10-22 14:47:04 +0530

    What is this?

---
+++ chapter6array/whatIsThis.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define SIZE 6
+
+// function prototype
+int whatIsThis(const int b[], size_t p); 
+
+// function main begins program execution
+int main(void)
+{ 
+    int x; // holds return value of function whatIsThis
+
+    // initialize array a
+    int a[SIZE] = { 1, 2, 3, 4, 5, 6};
+    x = whatIsThis(a, SIZE);
+    printf("Result is %d\n", x);
+} 
+
+int whatIsThis(const int b[], size_t p)
+{
+    // base case
+    if (1 == p) {
+        return b[0];
+    }
+    else { // recursion step
+        return b[p - 1] * whatIsThis(b, p - 1);
+    } 
+} 
